436b5756b2cf3346a5c78e52d5d3ac9a4bc76797
黄为宏 <13272618737@163.com>
2023-10-17 22:48:19 +0800

    第二版手机号生成

---
+++ script.js
@@ -0,0 +1,5 @@
+function generatePhoneNumber(countryCode) {
+    // Implement the logic to generate a phone number for the specified country
+    // You can redirect to a new page with the generated phone number
+    window.location.href = `${countryCode}-phone-number-generator`;
+}
