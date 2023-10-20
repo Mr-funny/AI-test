d57841c65548e5a9d747d1ca2defda789a0cd81d
黄为宏 <13272618737@163.com>
2023-10-20 20:54:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// base64Decode.js
-
 function customBase64Decode(base64String) {
-    try {
-        // Decoding standard Base64
-        const decoded = atob(base64String);
-        return decoded;
-    } catch (error) {
-        // If there are non-base64 characters, use decodeURIComponent
-        const decoded = decodeURIComponent(escape(atob(base64String)));
-        return decoded;
+    const binaryString = window.atob(base64String);
+    const bytes = new Uint8Array(binaryString.length);
+
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
     }
+
+    const text = new TextDecoder().decode(bytes);
+    return text;
 }
 
 // 示例用法
-// const base64String = '5YyX5Lqs6KW/5LqM5peX5ZKM5LiK5rW35byg5rGf56iL5bqP5ZGY55qE57uI5p6B5oKy5oOo5ZG96L+Q';
-// const decodedString = customBase64Decode(base64String);
-// console.log(decodedString);
+const base64String = '5YyX5Lqs6KW/5LqM5peX5ZKM5LiK5rW35byg5rGf56iL5bqP5ZGY55qE57uI5p6B5oKy5oOo5ZG96L+Q';
+const decodedString = customBase64Decode(base64String);
+console.log(decodedString);
